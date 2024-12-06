b011607dad7e7f73c2364477f641703533fc63b5
miglesuminaite <159789395+miglesuminaite@users.noreply.github.com>
2024-12-06 18:59:22 +0200

    Pakeitimai

--- react-app/src/components/Home.jsx
+++ react-app/src/components/Home.jsx
@@ -7,8 +7,7 @@ function Home() {
         <h1>Blizgi Rožė</h1>
         <p>
           Blizgi Rožė specializuojasi kuriant išskirtines gyvų blizgių rožių puokštes ypatingoms progoms.
-          Kiekviena puokštė kruopščiai sukurta, kad suteiktų džiaugsmą ir eleganciją.
-        </p>
+          Kiekviena puokštė kruopščiai sukurta, kad suteiktų džiaugsmą ir eleganciją.</p>
         <h3>Mūsų teikiamos paslaugos:</h3>
         <ul>
           <li>Gyvų rožių puokščių kūrimas</li>
